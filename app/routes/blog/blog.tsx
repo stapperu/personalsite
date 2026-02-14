eb937cba9e9de4f7c630efb904b63f1aa6954050
stapperu <k.kubiak94@gmail.com>
2026-02-14 17:58:14 +0100

    added pagination to blog posts too

--- app/routes/blog/blog.tsx
+++ app/routes/blog/blog.tsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import type {Route} from "./+types/blog";
 // import type { article } from "~/types";
 import { FiArrowUpRight } from "react-icons/fi";
 import { FiBox } from "react-icons/fi";
 import {useLoaderData,Link} from "react-router"
 
+
 export async function loader({request}:Route.LoaderArgs):Promise<any>{
   const url=new URL(`/data/blogPosts.json`,request.url)
   const res=await fetch(url);
-  const blogPosts=await res.json();
-    return blogPosts;
+  const blogArticles=await res.json();
+    return blogArticles;
 }
 
 const BlogPage = ({loaderData}:Route.ComponentProps)=>{
   const {blogArticles}=loaderData;
+
+const [currentPage,SetCurrentPage]=useState(1);
+const itemsPerPage=4
+const totalPages=Math.ceil(blogArticles.length/itemsPerPage);
+const indexOfLast=currentPage*itemsPerPage;
+const indexOfFirst=indexOfLast-itemsPerPage;
+const currentArticles=blogArticles.slice(indexOfFirst,indexOfLast)
+
+const renderPaginationButtons = () => (
+  <div className="paginationSection flex justify-center gap-2 mt-8">
+{Array.from({length:totalPages},(_,index)=>(
+  <button key={index+1} className="px-3 py-1 cursor-pointer rounded z-10" onClick={()=>SetCurrentPage(index+1)}>{index+1}</button>
+))}
+
+  </div>
+      
+)
 
   return (
     <>
     <FiBox className=" fixed w-1/2 h-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 opacity-8 text-yellow-100"/>
       <h1>BIG articleS</h1>
     <div className="grid gap-4 md:grid-cols-2 p-4">
-      {blogArticles.map((article:any)=>(
+      {currentArticles.map((article:any)=>(
          <Link to={`/blog/${article.id}`} key={article.id}>
         <div className="bg-gray-900 m-2 pt-8 p-8 text-yellow-50 shadow-[0px_-2px_2px_yellow] z-5 opacity-85">
             <h2 className="text-yellow-300 text-xl border-yellow-300">{article.headline}</h2>
@@ -31,7 +50,9 @@ const BlogPage = ({loaderData}:Route.ComponentProps)=>{
             
             </div></Link>
       ))}
-    </div></>
+    </div>
+    {totalPages>1 && renderPaginationButtons()}
+    </>
   )
 }
 
